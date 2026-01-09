99957c79fbb841812616f19acafc53d26e71c5c6
Marco Antonio Reyes Del Ángel <marda_da-capo@hotmail.com>
2026-01-08 19:59:06 -0600

    Beggins Layered Architecture

---
+++ src/main/java/com/marda/administrative_authorization_language_courses_layered/commons/entity/base/BaseEntity.java
@@ -0,0 +1,60 @@
+package com.marda.administrative_authorization_language_courses_layered.commons.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@AllArgsConstructor
+public class BaseEntity {
+    @Column(name = "aud_register_date", nullable = false)
+    private LocalDateTime audRegisterDate;
+
+    @Column(name = "aud_register_id_user", nullable = false)
+    private Long audRegisterIdUser;
+
+    @Column(name = "aud_register_ip", nullable = false)
+    private String audRegisterIp;
+
+    @Column(name = "state", nullable = false, length = 1)
+    private String state;
+
+    public BaseEntity() {
+    }
+
+    public LocalDateTime getAudRegisterDate() {
+        return audRegisterDate;
+    }
+
+    public void setAudRegisterDate(LocalDateTime audRegisterDate) {
+        this.audRegisterDate = audRegisterDate;
+    }
+
+    public Long getAudRegisterIdUser() {
+        return audRegisterIdUser;
+    }
+
+    public void setAudRegisterIdUser(Long audRegisterIdUser) {
+        this.audRegisterIdUser = audRegisterIdUser;
+    }
+
+    public String getAudRegisterIp() {
+        return audRegisterIp;
+    }
+
+    public void setAudRegisterIp(String audRegisterIp) {
+        this.audRegisterIp = audRegisterIp;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/com/marda/administrative_authorization_language_courses_layered/commons/service/base/BaseServiceImpl.java
@@ -0,0 +1,34 @@
+package com.marda.administrative_authorization_language_courses_layered.commons.service.base;
+
+import com.marda.administrative_authorization_language_courses_layered.commons.service.exception.ServiceException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public class BaseServiceImpl<T, R> implements BaseService<T, R> {
+    @Override
+    public Page<R> findLikeObject(T t, Pageable pageable) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public Optional<R> findById(Long id) throws ServiceException {
+        return Optional.empty();
+    }
+
+    @Override
+    public Long save(T r) throws ServiceException {
+        return 0L;
+    }
+
+    @Override
+    public Boolean update(T t) throws ServiceException {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(T t) throws ServiceException {
+        return null;
+    }
+}
